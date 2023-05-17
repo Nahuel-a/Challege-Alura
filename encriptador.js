62c3d45b01b598b0ca35b3ee82d3fefb6d9cf8b2
nahuel arrieta <nahuel.94.arrieta@gmail.com>
2023-05-17 11:52:23 -0300

    Optimización del codigo Js - V1.5

--- encriptador.js
+++ encriptador.js
@@ -1,26 +1,23 @@
-const textArea = document.querySelector("textArea");
+const entradaTexto = document.querySelector("textArea");
 const btnCopiar = document.getElementById("btn-copiar");
 const contenedorImg = document.getElementById("contenedor-derecha");
 const contenedorEncriptador = document.getElementById("contenedor-encriptador");//
 const areaTextoEncriptado = document.getElementById("texto-encriptado");
 const mensajeCopiado = document.getElementById("mensaje-copiado");
-
-const ventadaAvertencia = document.getElementById("ventana-advertencia");
+const ventanaAdvertencia1 = document.getElementById("ventana-advertencia1");
 const btnCerrar = document.getElementById("cerrar-ventana");
+const ventanaAdvertencia2 = document.getElementById("ventana-advertencia2");
+const caracteresEspeciales = /[^a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]/g;
 
-btnCopiar.addEventListener("click", function(){
+btnCopiar.addEventListener("click", () => {
     navigator.clipboard.writeText(areaTextoEncriptado.innerText);
 
     mensajeCopiado.style.display = "block";
-    setTimeout(function(){
-        mensajeCopiado.style.display = "none";
-    }, 1500);
-    
-    textArea.value="";
+    setTimeout(() => mensajeCopiado.style.display = "none", 1000);
+    entradaTexto.value="";
 });
 
-
-textArea.addEventListener("input", function(){
+entradaTexto.addEventListener("input", function() {
     if(this.value === ""){
         areaTextoEncriptado.innerText = "";
         contenedorImg.style.display = "flex";
@@ -29,72 +26,71 @@ textArea.addEventListener("input", function(){
 });
 
 async function btnEncriptar(){
-    let textoEncriptado = await validaciones(textArea.value);
-    textoEncriptado = encriptar(textoEncriptado);
-    
-    if(textoEncriptado != ''){
+    try {
+        const textoEncriptado = await validarTexto(entradaTexto.value);
         mostrarContenido();
-        areaTextoEncriptado.innerText = textoEncriptado;
+        areaTextoEncriptado.innerText = encriptarTexto(textoEncriptado);
+
+    } catch (error) {
+        mostrarError();
     }
 }
 
 async function btnDesencriptar(){
-    let textoDesencriptado = await validaciones(textArea.value);
-    textoDesencriptado = desencriptar(textoDesencriptado);
-    if(textoDesencriptado != ''){
+    try {
+        const textoDesencriptado = await validarTexto(entradaTexto.value);
         mostrarContenido();
-        areaTextoEncriptado.innerText = textoDesencriptado;
+        areaTextoEncriptado.innerText = desencriptarTexto(textoDesencriptado);
+
+    } catch (error) {
+        mostrarError();
     }
 }
 
-function encriptar(stringEncriptado){
-    
-    stringEncriptado = stringEncriptado.replace(/e/gi, "enter")
-            .replace(/i/gi,"imes" )
-            .replace(/a/gi, "ai")
-            .replace(/o/gi, "ober")
-            .replace(/u/gi, "ufat"); 
-
-    return stringEncriptado;
+function encriptarTexto(textoEncriptado){
+    return textoEncriptado.replace(/e/gi, "enter")
+        .replace(/i/gi,"imes" )
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat"); 
 }
 
-function desencriptar(stringDesencriptado){
-
-    stringDesencriptado = stringDesencriptado.replace(/enter/gi, "e")
-            .replace(/imes/gi,"i" )
-            .replace(/ai/gi, "a")
-            .replace(/ober/gi, "o")
-            .replace(/ufat/gi, "u"); 
-
-   return stringDesencriptado;
+function desencriptarTexto(textoDesencriptado){
+    return textoDesencriptado.replace(/enter/gi, "e")
+        .replace(/imes/gi,"i" )
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
 }
 
-async function validaciones(encripDesencrip){
-    let carecteresEspeciales = /[^a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]/g;
-    
+async function validarTexto(encripDesencrip){    
     encripDesencrip = encripDesencrip.toLowerCase();
-
+    
     if (encripDesencrip === ""){
-        alert("Debes de ingresar texto para Encriptar/Desencriptar");
+        return Promise.reject();
+    }
 
-    }else if(carecteresEspeciales.test(encripDesencrip)){
-        ventadaAvertencia.classList.remove("hidden");
+    if(caracteresEspeciales.test(encripDesencrip)){
+        ventanaAdvertencia1.classList.remove("hidden");
 
-        await new Promise(resolve => {
-
-            btnCerrar.addEventListener("click", async () => {
-                ventadaAvertencia.classList.add("hidden");
-                resolve();
+        return new Promise((resolve) => {
+            btnCerrar.addEventListener("click", () => {
+                ventanaAdvertencia1.classList.add("hidden");
+                resolve(encripDesencrip.normalize("NFD").replace(caracteresEspeciales,''));
             });
         });
-        encripDesencrip = encripDesencrip.normalize("NFD")
-        .replace(carecteresEspeciales,'');
     }
-
-    return encripDesencrip;
+    return Promise.resolve(encripDesencrip);
 }
 
 function mostrarContenido(){
     contenedorImg.style.display = "none";
     contenedorEncriptador.style.display = "block";
+}
+
+function mostrarError(){
+    ventanaAdvertencia2.classList.remove("hidden");
+    setTimeout(() => {
+       ventanaAdvertencia2.classList.add("hidden");
+    }, 1500);
 }
